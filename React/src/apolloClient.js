937a42c48ba5839872b77ff42294587bdc10fbca
MishailDeveloper <miha3xp@gmail.com>
2025-02-28 18:49:32 +1100

    Обновление файлов

--- React/src/apolloClient.js
+++ React/src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://192.168.0.177:3000/api/graphql',
+  uri: 'http://Поменяйте айпи здесь:3000/api/graphql',
   cache: new InMemoryCache(),
 });
 
